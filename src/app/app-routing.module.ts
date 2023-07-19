74ab80adb6340308e44490705931464f3883c81b
ninapantelic <nina.pantelic20@gmail.com>
2023-07-19 16:24:48 +0200

    Azuriran login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+
+  redirectTo: 'login',
+    pathMatch: 'full'
   },
   {
     path: 'login',
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AngularFireAuth } from "@angular/fire/compat/auth";
+import { User } from '../models/user.model';
+import { ToastController, LoadingController, NavController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  user = {} as User;
+
+  constructor(
+    private toast: ToastController,
+    private load: LoadingController,
+    private af: AngularFireAuth,
+    private nav: NavController,
+    private platform: Platform
+  ) { }
 
   ngOnInit() {
   }
+  async loginUser(user: User) {
+    if (this.formValidation()) {
+    
+
+      let loader = await this.load.create({
+        message: "Please wait",
+        spinner: "bubbles"
+      });
+      loader.present();
+      try {
+       
+        await this.af
+          .signInWithEmailAndPassword(user.email, user.password)
+          .then(data => {
+            console.log(data);
+
+           
+            this.nav.navigateRoot("home");
+          })
+          .catch();
+      } catch (e) {
+        this.showToast(e);
+      }
+      loader.dismiss();
+    }
+  }
+
+  formValidation() {
+    if (!this.user.email) {
+      this.showToast("Please, insert your email");
+      return false;
+    }
+
+    if (!this.user.password) {
+      this.showToast("Please, insert your password");
+      return false;
+    }
+    return true;
+  }
+
+  showToast(message: any) {
+    this.toast
+      .create({
+        message: message,
+        duration: 3000
+      })
+      .then(toastData => toastData.present());
+  }
+
+
+
+
 
 }
